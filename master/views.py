8b36ae1d7b25589dd665d90715d88c800c2e54b3
vishnuipr <vishnupv@ipressroom.com>
2020-06-02 23:33:56 +0530

    added file inside

--- master/views.py
+++ master/views.py
@@ -5,7 +5,10 @@ from django.http import HttpResponse
 
 
 def csv_view(request, id=None):  # function for converting json to csv file
-    if id:
+
+    if id == 'file':
+        search = 'master/71021091000190.json'
+    elif id:
         search = "http://receitaws.com.br/v1/cnpj/"+str(id)
     else:
         search = "http://receitaws.com.br/v1/cnpj/71021091000190"
@@ -14,10 +17,23 @@ def csv_view(request, id=None):  # function for converting json to csv file
     response['Content-Disposition'] = 'attachment; filename="Search Results.csv"'
     writer = csv.writer(response)  # Creating the csv writer object
     # response = HttpResponse("success")
-
-    with urllib.request.urlopen(search) as url:
-        data = json.loads(url.read().decode())
+    if id == 'file':
+        file = open(search)
+        data = json.load(file)
         if data is not None:
             writer.writerow(data.keys())  # Writing header
             writer.writerow(data.values())  # Writing values
+
+
+    else:
+        try:
+            with urllib.request.urlopen(search) as url:
+                data = json.loads(url.read().decode())
+                if data is not None:
+                    writer.writerow(data.keys())  # Writing header
+                    writer.writerow(data.values())  # Writing values
+
+        except:
+            return HttpResponse("Url Not found")
+
     return response
