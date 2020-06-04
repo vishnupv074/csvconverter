41652a3c95d24866a8ed98ecae14323c5be20983
vishnuipr <vishnupv@ipressroom.com>
2020-06-04 19:15:36 +0530

    New interface

--- master/views.py
+++ master/views.py
@@ -3,6 +3,7 @@ import json
 import csv
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.utils.encoding import smart_str
 
 
 def csv_view(request, id=None):  # function for converting json to csv file
@@ -36,39 +37,39 @@ def csv_view(request, id=None):  # function for converting json to csv file
             return response
 
 
-    else:
+    # else:
+    #
+    #     if id == 'file':
+    #         search = 'master/71021091000190.json'
+    #     elif id:
+    #         search = "http://receitaws.com.br/v1/cnpj/"+str(id)
+    #     else:
+    #         search = "http://receitaws.com.br/v1/cnpj/71021091000190"
+    #
+    #     response = HttpResponse(content_type='text/csv')  # Returns the csv file created
+    #     response['Content-Disposition'] = 'attachment; filename="Search Results.csv"'
+    #     writer = csv.writer(response)  # Creating the csv writer object
+    #     # response = HttpResponse("success")
+    #     if id == 'file':
+    #         file = open(search)
+    #         data = json.load(file)
+    #         if data is not None:
+    #             writer.writerow(data.keys())  # Writing header
+    #             writer.writerow(data.values())  # Writing values
+    #
+    #
+    #
+    #     else:
+    #         try:
+    #             with urllib.request.urlopen(search) as url:
+    #                 data = json.loads(url.read().decode())
+    #                 if data is not None:
+    #                     writer.writerow(data.keys())  # Writing header
+    #                     writer.writerow(data.values())  # Writing values
+    #
+    #             return response
+    #
+    #         except:
+    #             return HttpResponse("Url Not found")
 
-        if id == 'file':
-            search = 'master/71021091000190.json'
-        elif id:
-            search = "http://receitaws.com.br/v1/cnpj/"+str(id)
-        else:
-            search = "http://receitaws.com.br/v1/cnpj/71021091000190"
-
-        response = HttpResponse(content_type='text/csv')  # Returns the csv file created
-        response['Content-Disposition'] = 'attachment; filename="Search Results.csv"'
-        writer = csv.writer(response)  # Creating the csv writer object
-        # response = HttpResponse("success")
-        if id == 'file':
-            file = open(search)
-            data = json.load(file)
-            if data is not None:
-                writer.writerow(data.keys())  # Writing header
-                writer.writerow(data.values())  # Writing values
-
-
-
-        else:
-            try:
-                with urllib.request.urlopen(search) as url:
-                    data = json.loads(url.read().decode())
-                    if data is not None:
-                        writer.writerow(data.keys())  # Writing header
-                        writer.writerow(data.values())  # Writing values
-
-                return response
-
-            except:
-                return HttpResponse("Url Not found")
-
-    return render(request, 'index.html', {response:response})
+    return render(request, 'index.html', {'response': response})
